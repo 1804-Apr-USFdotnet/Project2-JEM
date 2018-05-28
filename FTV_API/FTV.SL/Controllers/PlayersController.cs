73934e9fce3c26632be45264b8623388be5048a8
Unknown <john.dominguez@stonybrook.edu>
2018-05-28 04:25:27 -0400

    Added Fortnite API controller to access player stats

---
+++ FTV_API/FTV.SL/Controllers/PlayersController.cs
@@ -0,0 +1,20 @@
+﻿using ForniteApi;
+using System.Threading.Tasks;
+using System.Web.Http;
+using FTV.BL;
+
+namespace FTV.SL.Controllers
+{
+    public class PlayersController : ApiController
+    {
+        // GET: api/Player
+        [HttpGet]
+        [Route("~/api/Players/{username}")]
+        public async Task<IHttpActionResult> Get(string userName)
+        {
+            var player = await FortniteApi.GetPlayerData(userName);
+
+            return Ok(player);
+        }
+    }
+}
---
+++ FTV_API/ForniteApi/FortniteApi.cs
@@ -0,0 +1,130 @@
+﻿using FortniteApi;
+using FortniteApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FortniteApi.Response;
+
+namespace ForniteApi
+{
+    public class FortniteApi
+    {
+        public static async Task<FortnitePlayer> GetPlayerData(string username)
+        {
+            ProfileResponse response;
+            using (FortniteClient client = new FortniteClient("bb93b0a2-0a92-4969-89aa-5180e589a065"))
+            {
+                response = await client.FindPlayerAsync(Platform.Pc, username);
+            }
+            return new FortnitePlayer(response);
+        }
+    }
+
+    public class FortnitePlayer
+    {
+        public string Username { get; set; }
+        public string Platform { get; set; }
+        public List<KeyValuePair<string, string>> LifeTimeStats { get; set; }
+        public List<Match> RecentMatches { get; set; }
+
+        public FortnitePlayer(ProfileResponse data)
+        {
+            PopulateData(data);
+        }
+
+        private void PopulateData(ProfileResponse data)
+        {
+            Username = data.EpicUserHandle;
+            Platform = data.PlatformNameLong;
+            LifeTimeStats = new List<KeyValuePair<string, string>>();
+            
+            foreach (var stat in data.LifeTimeStats)
+            {
+                LifeTimeStats.Add(new KeyValuePair<string, string>(stat.Key, stat.Value));
+            }
+
+            RecentMatches = new List<Match>();
+            foreach (var stat in data.RecentMatches)
+            {
+                RecentMatches.Add(new Match(stat));
+            }
+        }
+
+        public class Match
+        {
+            private int Matches { get; set; }
+            public int Kills { get; set; }
+            public int Score { get; set; }
+            public double Rating { get; set; }
+            public double RatingChange { get; set; }
+            public string Composition { get; set; }
+            public int Top1 { get; set; }
+            public int Top3 { get; set; }
+            public int Top5 { get; set; }
+
+            public Match(ProfileMatch stats)
+            {
+                Matches = stats.Matches;
+                Kills = stats.Kills;
+                Score = stats.Score;
+                Rating = stats.TrnRating;
+                RatingChange = stats.TrnRatingChange;
+                Composition = stats.Playlist;
+                Top1 = stats.Top1;
+                Top3 = stats.Top3;
+                Top5 = stats.Top5;
+            }
+        }
+    }
+}
+
+/* 
+ Stats worth it
+     PlatformId
+     PlatformName
+     EpicUserHandle
+     LifeTimeStats
+         0
+             Key
+             Value
+         ...
+         7
+             Key: //Matches played
+         8
+             Key: //Wins
+         9
+             Key: //Win%
+         10
+             Key: //kills
+         11
+             Key: // k/d
+
+    RecentMatches: // Needed to turn into a dataTable
+        0:
+            Playlist //Type of gamemode(solo/duo/squad)
+            Kills
+            DateCollected
+            Platform
+            Matches //# of matches on this day
+            Scored
+
+     P2: // global Duo stats
+         kd:
+             ValueDec
+             Rank
+             Percentile
+         winRatio:
+             ValueDec
+             Rank
+             Percentile
+         matches:
+             ValueInt
+             Percentile
+         kills:
+             valueInt
+         kpg: 
+             Label //kills per game
+             ValueInt
+         top1:
+             ValueInt
+ */
