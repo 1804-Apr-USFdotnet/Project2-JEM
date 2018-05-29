9eba71ef58b223c74710b6eace0ed0bb3f1a084f
Unknown <john.dominguez@stonybrook.edu>
2018-05-29 07:46:54 -0400

    Tried making angular not look like crap

--- FTV.Angular/angular-FTV-Angular/src/app/app.component.ts
+++ FTV.Angular/angular-FTV-Angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
